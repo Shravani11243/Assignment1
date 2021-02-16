0098e6c324dda0ce17547c4167d87f5374c2bc5d
Shravani11243 <shravanipatel3@gmail.com>
2021-02-15 23:32:17 -0500

    Added six methods,default constructor and non default constructor

--- Rectangle/Rectangle.cs
+++ Rectangle/Rectangle.cs
@@ -6,7 +6,48 @@ using System.Threading.Tasks;
 
 namespace Rectangle
 {
-    class Rectangle
+    class RectangleClass
     {
+        private int length_rectngle;
+        private int width_rectngle;
+
+        //Default comstructor
+        public  RectangleClass()
+        {
+         length_rectngle = 1;
+         width_rectngle = 1;
+    }
+        //
+        public RectangleClass(int givenLength,int givenWidth)
+        {
+            length_rectngle = givenLength;
+            width_rectngle = givenWidth;
+        }
+        //six Methods
+        public int GetWidth()
+        {
+            return width_rectngle;
+        }
+        public int GetLength()
+        {
+            return length_rectngle;
+        }
+        public int SetLength( int length)
+        {
+            return length_rectngle = length;
+        }
+        public int SetWidth(int width)
+        {
+            return length_rectngle = width;
+        }
+        public int GetPerimeter()
+        {
+            int Perimeter = length_rectngle + width_rectngle;
+            return (2 * (Perimeter));
+        }
+        public int GetArea()
+        {
+            return (length_rectngle * width_rectngle);
+        }
     }
 }
